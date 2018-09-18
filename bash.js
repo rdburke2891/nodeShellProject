b899370a36ef95d1069fa7e7ad91300ab2d261b5
Roy Yeh <royyeh007@gmail.com>
2018-09-18 11:28:17 -0500

    sdfjlsdk

---
+++ bash.js
@@ -0,0 +1,14 @@
+// Output a prompt
+process.stdout.write('prompt > ');
+
+//The stdin 'data' event fires after a user types in a line
+process.stdin.on('data', (data) =>{
+    const cmd = data.toString().trim();   //remove the newline
+    if(cmd === "pwd"){
+        process.stdout.write(process.cwd());
+        
+    }
+    process.stdout.write('\nprompt >');
+
+
+});
